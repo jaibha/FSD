877776d0259ed6e23589df827f16fad05ac1df68
JayaBharathi Selvakumar <jb17393@gmail.com>
2021-06-05 23:17:03 +0530

    added arithmetic prg

--- FSD-training/src/HelloWorld.java
+++ FSD-training/src/HelloWorld.java
@@ -2,13 +2,16 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-System.out.println("Hello World");
-System.out.println("Hello World!!");
-System.out.println(args.length); // size of an array
-System.out.println(args[0] + args[1]);
-int a= Integer.parseInt(args[0]);
-int b= Integer.parseInt(args[1]);
-System.out.println(a+b);
+		System.out.println("Hello World");
+		System.out.println("Hello World!!");
+		System.out.println(args.length); // size of an array
+		System.out.println(args[0] + args[1]);
+		int a= Integer.parseInt(args[0]);
+		int b= Integer.parseInt(args[1]);
+		System.out.println("Addition is" + " " + (a+b));
+		System.out.println("Substraction is" + " " + (a-b));
+		System.out.println("Multiplication is" + " " + (a*b));
+		System.out.println("Division is" + " " + (a/b));
 	}
 
 }
